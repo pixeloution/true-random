0cccc0f387ff3a88da80894bfe9e47b59ae60bf6
pixeloution <erik@pixeloution.com>
2013-01-31 11:49:16 -0600

    changed to non-static methods, added sequences
    * code igniter doesn't play well with mostly-static classes; changed
    to a instantiated class.
    * added UA string to outgoing requests
    * added access to random.org's sequence generator
    * added rnd=new to requests to get non-predetermined numbers

--- random.php
+++ random.php
@@ -4,18 +4,40 @@
  * limitaions set forth by random.org
  *
  * Example: 
- * foreach( TrulyRandom::get(0,1000, 2) as $integer ) {
- *    echo $integer . "\n";
- * }
+ * later
+ * 
  * 
  */
-class TrulyRandom
+class Random
 {
-   protected static $url   = 'http://www.random.org/integers/';
-   protected static $query = '?num=%d&min=%d&max=%d&col=1&base=10&format=plain';
+   protected static $url = 'http://www.random.org';
+   protected static $int = '/integers/?num=%d&min=%d&max=%d&col=1&base=10&format=plain&rnd=new';
+   protected static $seq = '/sequences/?min=%d&max=%d&col=1&format=plain&rnd=new';
+   
+   # set the UserAgent; random.org recommends your email address be in the UA string
+   protected $ua  = '';
    
    /**
-    * Gets Random Integer From API
+    * makes sure we have CURL installed
+    *
+    * TODO: perhaps dependency injection for code that actually gets stuff from
+    * the internets
+    */
+   public function __construct( $opts )
+   {
+      #
+      # dependency
+      #
+      if( ! function_exists('curl_version') )
+         throw new Exception('PHP CURL must be enabled on server to use Random::get');      
+
+      # set the user agent if provided
+      if( $opts['ua'] )
+         $this->ua = $opts['ua'];
+   }
+
+   /**
+    * Gets Random Integer(s) From API
     * 
     * @param  int $min
     * @param  int $max
@@ -23,23 +45,42 @@ class TrulyRandom
     * 
     * @return array
     */
-   public static function get( $min, $max, $quantity = 1 )
+   public function int( $min, $max, $quantity = 1 )
    {  
-      #
-      # dependency
-      #
-      if( ! function_exists('curl_version') )
-         throw new Exception('PHP CURL must be enabled on server to use TrulyRandom::get');
-
       #
       # build HTTP request URL
       # 
-      $url = self::$url . sprintf( self::$query, $quantity, $min, $max );
+      $url = self::$url . sprintf( self::$int, $quantity, $min, $max );
 
       #
       # get the numbers from random.org
       # 
-      $raw  = self::get_data( $url );
+      $raw  = $this->_get_data( $url );
+      $ints = $this->_parse( $raw ); 
+
+      return $ints;
+   }
+
+
+   public function sequence( $start, $end )
+   {
+      #
+      # build HTTP request URL
+      # 
+      $url = self::$url . sprintf( self::$seq, $start, $end );
+
+      #
+      # get the numbers from random.org
+      # 
+      $raw  = $this->_get_data( $url );
+      $ints = $this->_parse( $raw ); 
+
+      return $ints;   
+   }
+
+
+   protected function _parse( $raw )
+   {
       $ints = explode( "\n", trim( $raw ) );
 
       # guard against odd data being returned ...
@@ -62,14 +103,20 @@ class TrulyRandom
     * 
     * @return string
     */
-   private static function get_data( $url )
+   protected function _get_data( $url )
    {
       $CH = curl_init( $url );
       
       curl_setopt( $CH, CURLOPT_RETURNTRANSFER, true );
+      # random.org recommends setting a high timeout for applications that can
+      # happily do so without issues
+      curl_setopt( $CH, CURLOPT_CONNECTTIMEOUT, 60   );
+      # random.org requests you include your email address in the user agent
+      # http://www.random.org/clients/
+      curl_setopt( $CH, CURLOPT_USERAGENT, $this->ua );
+
       $data = curl_exec( $CH );
 
-      
       #
       # make sure we actually got data
       # 
@@ -80,5 +127,3 @@ class TrulyRandom
       return $data;
    }
 }
-
-
