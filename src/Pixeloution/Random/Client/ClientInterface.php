a75a2079eb83effc36b5fabae2b6c7f95a1aafb3
Erik Wurzer <erik@pixeloution.com>
2013-07-20 22:11:36 -0500

    Get rid of these ugly hacks now that Guzzle actually has an interface for the http client

--- src/Pixeloution/Random/Client/ClientInterface.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Pixeloution\Random\Client;
-
-
-interface ClientInterface
-{
-   public function get( $uri );
-   public function setUserAgent( $uastring );
-   public function setBaseUrl( $url );
-}
